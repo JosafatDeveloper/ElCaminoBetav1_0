de0972773e441046d0b5f7d3352cfe5ddaa40d30
Josafat Moises Vargas Origuela <jos_var@live.com.mx>
2016-10-07 18:04:17 -0500

    New GPS and Sensor

--- app/src/main/java/mx/onlinesellers/elcaminobeta10/ManagerSQLiteHelper.java
+++ app/src/main/java/mx/onlinesellers/elcaminobeta10/ManagerSQLiteHelper.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class ManagerSQLiteHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "ELCBD.db";
-    public static final int DATABASE_VERSION = 4;
+    public static final int DATABASE_VERSION = 22;
 
     public ManagerSQLiteHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
